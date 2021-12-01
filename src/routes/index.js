f8d68d300e749183d7758da9c314b2f7e4956398
Anna W <tester.kodilla@gmail.com>
2021-12-01 10:40:00 +0000

    Swagger Annotations

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,77 @@
 const express = require('express');
-const {booksController} = require('../controllers');
+const { booksController } = require('../controllers');
 
 const router = new express.Router();
+
+/**
+ * @swagger
+ * /api/v1/books:
+ *  get:
+ *      summary: Get all books
+ *      responses:
+ *          200:
+ *              description: An array of books
+*/
 router.get('/books', booksController.getBooks);
+
+/**
+ * @swagger
+ * /api/v1/books/{bookId}:
+ *  get:
+ *      summary: Get a single book by bookId
+ *      parameters:
+ *        - in: path
+ *          name: bookId
+ *          schema:
+ *            type: integer
+ *          required: true
+ *          description: Numeric ID of the book to get
+ *      responses:
+ *          200:
+ *              description: A single book
+*/
 router.get('/books/:bookId', booksController.getBook);
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *       properties:
+ *         bookId:
+ *           type: integer
+ *           description: Auto-generated book id
+ *         title:
+ *           type: string
+ *           description: Book title
+ *         author:
+ *           type: string
+ *           description: Book author
+*/
+
+/**
+ * @swagger
+ * /api/v1/books:
+ *  post:
+ *      summary: Add a new book
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Book'
+ *      responses:
+ *        '201':
+ *          description: Created
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/Book'
+*/
 router.post('/books', booksController.saveBook);
 
 // User Story 4 - Update Book By Id Solution
